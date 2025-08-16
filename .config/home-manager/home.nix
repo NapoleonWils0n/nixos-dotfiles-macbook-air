64f095659d5c0762b42ed0e6b2b1e4f4677ed768
Daniel J Wilcox <danieljwilcox@gmail.com>
2025-08-16 13:50:16 +0100

    emacs treesitter refactored

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -32,23 +32,23 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  # emacs with treesitter
-  programs.emacs = {
-    enable = true;
-    package = (pkgs.emacsPackagesFor pkgs.emacs-pgtk).emacsWithPackages (epkgs: with epkgs; [
-      # Core Emacs treesitter package
-      tree-sitter
-  
-      # Treesitter grammars are added here with the proper function call
-      (treesit-grammars.with-grammars (grammars: with grammars; [
+  # emacs 
+  programs.emacs =
+    let
+      treesit-grammars = pkgs.emacsPackages.treesit-grammars.with-grammars (grammars: with grammars; [
         tree-sitter-bash
         tree-sitter-elisp
         tree-sitter-nix
         tree-sitter-python
-        # Add other grammars as needed
-      ]))
-    ]);
-  };
+      ]);
+    in
+    {
+      enable = true;
+      package = (pkgs.emacsPackagesFor pkgs.emacs-pgtk).emacsWithPackages (epkgs: with epkgs; [
+        (treesit-grammars)
+      ]);
+    };
+
 
   # --- OBS Studio Configuration for wlrobs ---
   programs.obs-studio = {
